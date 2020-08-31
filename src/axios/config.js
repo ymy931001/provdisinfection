b4c0b12c815addc7816e990a868d1827ae73c81f
ymy931001 <yumaoyang@terabits.cn>
2020-08-31 15:41:08 +0800

    8.31

--- src/axios/config.js
+++ src/axios/config.js
@@ -5,8 +5,8 @@
 // export const requestIp = "http://47.94.211.109:9090";
 // export const requestIp = "http://192.168.3.7:9090";
 // export const requestIp = "http://2695627dm6.qicp.vip";
-// export const requestIp = "http://iva.terabits.cn:9090";
-export const requestIp = "http://47.110.230.84:9090";
+export const requestIp = "http://iva.terabits.cn:9090";
+// export const requestIp = "http://47.110.230.84:9090";
 
 
 export const downUrl = requestIp//下载报告地址
